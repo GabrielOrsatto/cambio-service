29e17cea086b28483b8541e456a0a7f90a7bfb00
GABRIEL PRADEGAN ORSATTO <1135097@ATITUS.LOCAL>
2024-10-03 21:46:40 -0300

    commit 1

---
+++ src/main/java/br/edu/atitus/paradigma/cambio_service/repositories/CambioRepository.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.paradigma.cambio_service.repositories;
+
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
+
+@Repository
+public interface CambioRepository extends JpaRepository<CambioEntity, Integer>{
+	
+	Optional<CambioEntity> findByOrigemAndDestino(String origem, String destino);
+
+}
